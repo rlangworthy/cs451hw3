0f9fe059d43ac6734549066bd083ee2d96d23604
Chameleon Cloud User <cc@mpi-instance-0.novalocal>
2019-11-01 23:15:09 +0000

    q2

--- mpi-pi.c
+++ mpi-pi.c
@@ -1,17 +1,20 @@
 #include <mpi.h>
 #include <math.h>
+#include <stdio.h>
 
 int main(argc,argv)
 int argc;
 char *argv[];
 {
-    int done = 0, n, myid, numprocs, i;
+    int done = 0, n, myid, numprocs, i, maxrank;
     double PI25DT = 3.141592653589793238462643;
     double mypi, pi, h, sum, x;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD,&myid);
+    maxrank=numprocs-1;
+    printf ("Hello from task %d of %d !\n", myid, maxrank);
     while (!done)
     {
 	if (myid == 0) {
@@ -29,10 +32,10 @@ char *argv[];
 	}
 	mypi = h * sum;
     
-	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, 0,
+	MPI_Reduce(&mypi, &pi, 1, MPI_DOUBLE, MPI_SUM, maxrank,
 		   MPI_COMM_WORLD);
     
-	if (myid == 0)
+	if (myid == maxrank)
 	    printf("pi is approximately %.16f, Error is %.16f\n",
 		   pi, fabs(pi - PI25DT));
     }
