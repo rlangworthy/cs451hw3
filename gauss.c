9be73e593b7268eb141219ad2c8b978f47f80bfe
Robert Langworthy <robertlangworthy@MacBook-Pro.local>
2019-11-01 23:26:27 -0500

    asdf

--- gauss.c
+++ gauss.c
@@ -162,10 +162,8 @@ int main(int argc, char **argv) {
   MPI_Bcast(&A[0][0], MAXN*MAXN, MPI_FLOAT, 0, MPI_COMM_WORLD);
   MPI_Bcast(&B, MAXN, MPI_FLOAT, 0, MPI_COMM_WORLD);
   /* Gaussian elimination */
-  printf("hello %i\n", myid);
   for(norm = 0; norm < N - 1; norm++) {
     for (row = norm + 1 + myid; row < N; row += numprocs) {
-      printf("%i %i %i\n", norm, row, myid);
       multiplier = A[row][norm] / A[norm][norm];
       for (col = norm; col < N; col++) {
 	    A[row][col] -= A[norm][col] * multiplier;
--- mpi-pi.c
+++ mpi-pi.c
@@ -27,6 +27,7 @@ char *argv[];
 	h   = 1.0 / (double) n;
 	sum = 0.0;
 	for (i = myid + 1; i <= n; i += numprocs) {
+        printf("%i\n", myid);
 	    x = h * ((double)i - 0.5);
 	    sum += 4.0 / (1.0 + x*x);
 	}
