b341e5c53b3da7f77d9d20b542e65c771c4204cb
Robert Langworthy <robertlangworthy@MacBook-Pro.local>
2019-11-01 22:36:52 -0500

    gauss tests

--- gauss.c
+++ gauss.c
@@ -136,7 +136,7 @@ int main(int argc, char **argv) {
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD,&numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD,&myid);
-  if(myid=0){
+  if(myid==0){
     /* Process program parameters */
     parameters(argc, argv);
 
