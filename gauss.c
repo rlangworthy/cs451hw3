169fd3be2b1d43b70fc9c01fb7e43c1f87e10ffb
Robert Langworthy <robertlangworthy@MacBook-Pro.local>
2019-11-03 17:19:19 -0600

    scattergather

--- gauss.c
+++ gauss.c
@@ -185,7 +185,7 @@ int main(int argc, char **argv) {
     }
 
     for (row = norm + 1 + bdispl[myid]; row < norm + 1 + bdispl[myid] + bcounts[myid]; row ++) {
-        printf("row %i norm %i on %i, chunk size %i, N , div %i, %i\n", row, norm, myid, bcounts[myid], N, div, (div%numprocs));
+        printf("row %i norm %i on %i, chunk size %i, N %i, div %i, %i\n", row, norm, myid, bcounts[myid], N, div, (div%numprocs));
         multiplier = A[row][norm] / A[norm][norm];
         for (col = norm; col < N; col++) {
             A[row][col] -= A[norm][col] * multiplier;
